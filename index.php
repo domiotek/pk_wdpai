ccddca1cd8d8624af6e81148d183f2cd91f768f5
Damian <41295244+MrDanixx@users.noreply.github.com>
2023-11-22 11:46:44 +0100

    Basic routing

--- index.php
+++ index.php
@@ -1,14 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projekt WDPAI</title>
-</head>
-<body>
-    <?php
-        echo 'Hi there 👋';
-    ?>
-</body>
-</html>
+<?php
+
+
+require "Routing.php";
+
+$path = trim($_SERVER["REQUEST_URI"], "/");
+$path = parse_url($path, PHP_URL_PATH);
+
+Router::get("index", "DefaultController");
+Router::get("projects", "DefaultController");
+
+Router::run($path);
